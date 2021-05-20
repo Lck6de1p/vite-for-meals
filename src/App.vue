1fc7c413e1c0a65033ff1f3541a9aa9b711c8d77
Lck6de1p <310504719@qq.com>
2021-05-20 15:19:15 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,18 @@
 <script setup>
 import Ball from './components/Ball.vue'
 import { ref, onMounted, computed } from 'vue'
-const mealStr = ref('别吃别吃 馄饨 烩面 热干面 刀削面 油泼面 炸酱面 炒面 重庆小面 米线 酸辣粉 土豆粉 螺狮粉 凉皮儿 麻辣烫 肉夹馍 羊肉泡馍 炒饭 盖浇饭 烤肉饭 黄焖鸡米饭 麻辣香锅 火锅 酸菜鱼 烤串 披萨 烤鸭 汉堡 炸鸡 寿司 煎饼果子 南瓜粥 小龙虾 牛排 砂锅 大排档 馒头 西餐 自助餐 小笼包 水果 西北风 烧烤 泡面 水饺 日本料理 涮羊肉 兰州拉面 肯德基 面包 臊子面 小笼包 麦当劳 沙县小吃 烤鱼 海鲜 铁板烧 韩国料理 甜点 鸭血粉丝汤')
-const mealArr = ref([])
+const mealArr = ref(["别吃别吃", "锟斤拷", "馄饨", "烩面", "热干面", "刀削面", "油泼面", "炸酱面", "炒面", "重庆小面", "米线", "酸辣粉", "土豆粉", "螺狮粉", "凉皮儿", "麻辣烫", "肉夹馍", "羊肉泡馍", "炒饭", "盖浇饭", "烤肉饭", "黄焖鸡米饭", "麻辣香锅", "火锅", "酸菜鱼", "烤串", "披萨", "烤鸭", "汉堡", "炸鸡", "寿司", "煎饼果子", "南瓜粥", "小龙虾", "牛排", "砂锅", "大排档", "馒头", "西餐", "自助餐", "小笼包", "水果", "西北风", "烧烤", "泡面", "水饺", "日本料理", "涮羊肉", "兰州拉面", "肯德基", "面包", "臊子面", "小笼包", "麦当劳", "沙县小吃", "烤鱼", "海鲜", "铁板烧", "韩国料理", "甜点", "鸭血粉丝汤"])
 const ballArr = ref([])
 const mealName = ref('')
 const timerAdd = ref(null)
 const timerDel = ref(null)
 const timerClean = ref(null)
-const btnName = computed(() => {
-  return timerAdd.value === null ? '开始' : '停止'
-})
 const clientHeight = ref(0)
 const clientWidth = ref(0)
 
-
+const btnName = computed(() => {
+  return timerAdd.value === null ? '开始' : '停止'
+})
 onMounted(() => {
   const dom = document.getElementsByClassName('container')[0]
   clientWidth.value = dom.clientWidth
@@ -44,13 +42,12 @@ function stop() {
 function start() {
   clearTimeout(timerClean.value)
   timerAdd.value = setInterval(() => {
-    const mealArr = mealStr.value.split(' ')
-    const len = mealArr.length
-    mealName.value = mealArr[parseInt(Math.random() * len)]
+    const len = mealArr.value.length
+    mealName.value = mealArr.value[parseInt(Math.random() * len)]
     ballArr.value.push({
       top: parseInt(Math.random() * clientHeight.value) + 'px',
       left: parseInt(Math.random() * clientWidth.value) + 'px',
-      name: mealArr[parseInt(Math.random() * len)],
+      name: mealArr.value[parseInt(Math.random() * len)],
       fontSize: 12 + parseInt(Math.random() * 20) + 'px',
     })
   }, 100)
